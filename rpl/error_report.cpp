6d5720c8ccfcb1f79d78c94b4c9dfd8a9b222e34
Leonardo Gazzarri <leogazza91@gmail.com>
2017-04-08 16:14:55 +0200

    error reporting

---
+++ rpl/error_report.cpp
@@ -0,0 +1,76 @@
+#include "error_report.hpp"
+#include <string>
+
+using namespace std;
+
+// "pretty print" styles
+const style pp_err(color::err, font::bold);
+const style pp_def(color::def, font::normal);
+
+// common print func
+void print(ostream& os, const string& tok, const string& info, int pos) {
+    int offset = rplsh::name.size() + pos;
+    os << pp_err << string(offset, ' ');
+    os << pp_err << string( max<int>(tok.size(), 1), '^') << endl;
+    os << pp_def << rplsh::info;
+    os << pp_err << "error";
+    os << pp_def << ": token '" << tok << "' " << info << endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Implementation of the base class error
+//
+///////////////////////////////////////////////////////////////////////////////
+void error::print(ostream& os) const {
+    os << "error default\n";
+}
+
+ostream& operator<<(ostream& os, error const& err) {
+    err.print(os);
+    return os;
+}
+
+string error::line;
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Implementation of the derived class error_unexp
+//
+///////////////////////////////////////////////////////////////////////////////
+error_unexp::error_unexp(const string word, const int pos)
+    : word(word), pos(pos) {
+    }
+
+void error_unexp::print(ostream& os) const {
+    ::print(os, word, "is unexpected", pos);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Implementation of the derived class error_illegal
+//
+///////////////////////////////////////////////////////////////////////////////
+error_illegal::error_illegal(string word, int pos)
+    : word(word), pos(pos) {}
+
+void error_illegal::print(ostream& os) const {
+    ::print(os, word, "is illegal", pos);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Implementation of the class error_container
+//
+///////////////////////////////////////////////////////////////////////////////
+error& error_container::get(int pos) {
+    return *errors[pos];
+}
+
+void error_container::add(shared_ptr<error> err) {
+    errors.push_back(move(err));
+}
+
+int error_container::size() {
+    return errors.size();
+}
