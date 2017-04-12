3e3865832d8ae05bc560fc591b71edca233ec740
Leonardo Gazzarri <leogazza91@gmail.com>
2017-04-12 11:32:25 +0200

    added visitor

--- rpl/rplsh.cpp
+++ rpl/rplsh.cpp
@@ -7,41 +7,59 @@
 #include "interpreter.hpp"
 
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <locale>
 
 using namespace std;
 
-int main()
+typedef environment<string, shared_ptr<skel_node>> env_t;
+typedef dispatcher<string, skel_visitor> disp_t;
+typedef interpreter<env_t, disp_t> interpr_t;
+typedef lexer scanner_t;
+typedef parser parser_t;
+
+bool print_rpl() {
+    cout << rplsh::name;
+    return true;
+}
+
+bool is_empty_input(string& line) {
+    return line.empty() || line == string(line.length(), ' ');
+}
+
+bool is_quit_input(string& line) {
+    return line.length() == 1 && ( line[0] == 'q' || line[0] == 'Q' );
+}
+
+int main(int argc, char * argv[])
 {
-    string line;
-    environment env;
+    string line;    // input line
+    env_t env;      // environment: <name, skel_tree> bindings
+    disp_t disp;    // dispatcher: <name, skel_visitor> bindings
 
-    while (true)
+    // TODO don't use costant strings but invent some container class...
+    disp.add("show_default", make_shared<printer>());
+    disp.add("servicetime", make_shared<servicetime<env_t>>(env));
+    disp.add("latency", make_shared<latencytime<env_t>>(env));
+    while ( print_rpl() && getline(cin, line) )
     {
-        cout << rplsh::name;
-        getline(cin, line);
-
-        if (line.empty() || line == string(line.length(), ' '))
+        if (is_empty_input(line))
             continue;
-
-        //TODO command quit and command help
-        if (line.length() == 1 && ( line[0] == 'q' || line[0] == 'Q' ))
+        if (is_quit_input(line))
             break;
 
-        error_container err_repo;
-        lexer _lexer(line, err_repo);
-        parser _parser(_lexer, err_repo);
-        interpreter _interpr(env, err_repo);
+        error_container err_repo;                   // filled with errors
+        interpr_t _interpr(env, disp, err_repo);
+        scanner_t _scanner(line, err_repo);
+        parser_t _parser(_scanner, err_repo);
 
-        auto t = _parser.parse();
-
+        shared_ptr<rpl_node> t = _parser.parse();
         if (err_repo.size() == 0)
             t->accept(_interpr);
-        else
+        if (err_repo.size() > 0)
             cout << err_repo.get(0);
-
     }
 
     return 0;
--- rpl/visitor.hpp
+++ rpl/visitor.hpp
@@ -1,14 +1,16 @@
 #ifndef rpl_visitor_hpp
 #define rpl_visitor_hpp
 
+#include "environment.hpp"
 #include "rtree.hpp"
+#include <iostream>
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  The skeleton visitor abstract class
 //
 ///////////////////////////////////////////////////////////////////////////////
-struct skel_visitor
+struct base_visitor
 {
     virtual void visit(seq_node& n)     = 0;
     virtual void visit(comp_node& n)    = 0;
@@ -17,8 +19,13 @@ struct skel_visitor
     virtual void visit(map_node& n)     = 0;
     virtual void visit(reduce_node& n)  = 0;
     virtual void visit(id_node& n)      = 0;
+};
 
-    virtual void operator()(skel_node& sk){}
+struct skel_visitor : public virtual base_visitor
+{
+    using base_visitor::visit;
+    virtual void print()                    = 0;
+    virtual const skel_visitor& operator()(skel_node& sk)  = 0;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -26,9 +33,9 @@ struct skel_visitor
 //  The visitor abstract class
 //
 ///////////////////////////////////////////////////////////////////////////////
-struct visitor : public virtual skel_visitor
+struct visitor : public virtual base_visitor
 {
-    using skel_visitor::visit;
+    using base_visitor::visit;
     virtual void visit(assign_node& n) = 0;
     virtual void visit(show_node& n)   = 0;
     virtual void visit(set_node& n)    = 0;
@@ -40,11 +47,16 @@ struct visitor : public virtual skel_visitor
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Concrete visitors:
-//  servicetime, printer, ...
+//  servicetime, latencytime, ...
+//
+//  TODO: there is a way to reduce the boilerplate
+//  of code for defining the visitors?
 //
 ///////////////////////////////////////////////////////////////////////////////
+template <typename Env>
 struct servicetime: public skel_visitor
 {
+    servicetime(Env& env);
     virtual void visit(seq_node& n);
     virtual void visit(comp_node& n);
     virtual void visit(pipe_node& n);
@@ -52,18 +64,86 @@ struct servicetime: public skel_visitor
     virtual void visit(map_node& n);
     virtual void visit(reduce_node& n);
     virtual void visit(id_node& n);
-
-    virtual void operator()(skel_node& sk){
-            res = 0;
-            sk.accept(*this);
-    }
-
-    double get() const {
-        return res;
-    }
-
+    virtual void print();
+    virtual const skel_visitor& operator()(skel_node& sk);
 protected:
+    Env& env;
     double res;
+};
+
+template <typename Env>
+struct latencytime: public skel_visitor
+{
+    //using servicetime::visit;
+    latencytime(Env& env);
+    virtual void visit(seq_node& n);
+    virtual void visit(comp_node& n);
+    virtual void visit(pipe_node& n);
+    virtual void visit(farm_node& n);
+    virtual void visit(map_node& n);
+    virtual void visit(reduce_node& n);
+    virtual void visit(id_node& n);
+    virtual void print();
+    virtual const skel_visitor& operator()(skel_node& sk);
+protected:
+    Env& env;
+    double res;
+};
+
+template <typename Env>
+struct completiontime: public skel_visitor
+{
+    completiontime(Env& env);
+    void set_dimension(int dim);
+    virtual void visit(seq_node& n);
+    virtual void visit(comp_node& n);
+    virtual void visit(pipe_node& n);
+    virtual void visit(farm_node& n);
+    virtual void visit(map_node& n);
+    virtual void visit(reduce_node& n);
+    virtual void visit(id_node& n);
+    virtual void print();
+    virtual const skel_visitor& operator()(skel_node& sk);
+protected:
+    Env& env;
+    double res;
+    int dim;
+};
+
+template <typename Env>
+struct pardegree: public skel_visitor
+{
+    pardegree(Env& env);
+    virtual void visit(seq_node& n);
+    virtual void visit(comp_node& n);
+    virtual void visit(pipe_node& n);
+    virtual void visit(farm_node& n);
+    virtual void visit(map_node& n);
+    virtual void visit(reduce_node& n);
+    virtual void visit(id_node& n);
+    virtual void print();
+    virtual const skel_visitor& operator()(skel_node& sk);
+protected:
+    Env& env;
+    int res;
+};
+
+template <typename Env>
+struct resources: public skel_visitor
+{
+    resources(Env& env);
+    virtual void visit(seq_node& n);
+    virtual void visit(comp_node& n);
+    virtual void visit(pipe_node& n);
+    virtual void visit(farm_node& n);
+    virtual void visit(map_node& n);
+    virtual void visit(reduce_node& n);
+    virtual void visit(id_node& n);
+    virtual void print();
+    virtual const skel_visitor& operator()(skel_node& sk);
+protected:
+    Env& env;
+    int res;
 };
 
 struct printer : public skel_visitor
@@ -75,11 +155,10 @@ struct printer : public skel_visitor
     virtual void visit(map_node& n);
     virtual void visit(reduce_node& n);
     virtual void visit(id_node& n);
-
-    virtual void operator()(skel_node& sk){
-            sk.accept(*this);
-    }
+    virtual void print();
+    virtual const skel_visitor& operator()(skel_node& sk);
 private:
+    std::string res;
     void print(const std::string& name, const access_node& n);
 };
 
