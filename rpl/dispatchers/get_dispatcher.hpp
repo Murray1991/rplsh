565a49123b5c17cd5f363711a55c747cdaa8a7a5
Leonardo Gazzarri <leogazza91@gmail.com>
2017-05-23 11:18:05 +0200

    set verb implementation

---
+++ rpl/dispatchers/get_dispatcher.hpp
@@ -0,0 +1,21 @@
+#ifndef rpl_get_dispatcher_hpp
+#define rpl_get_dispatcher_hpp
+
+#include "dispatchers/dispatcher.hpp"
+#include "parameters.hpp"
+#include "rpl_environment.hpp"
+#include <functional>
+
+struct get_dispatcher : public dispatcher<std::string, std::function<double (void)>>
+{
+    get_dispatcher(rpl_environment& env) {
+        add(par::emitter_time,   [&]() { return env.get_emitter_time(); });
+        add(par::collector_time, [&]() { return env.get_collector_time(); });
+        add(par::scatter_time,   [&]() { return env.get_scatter_time(); });
+        add(par::gather_time,    [&]() { return env.get_gather_time(); });
+        add(par::resources,      [&]() { return env.get_res(); });
+        add(par::dimension,      [&]() { return env.get_dim(); });
+    }
+};
+
+#endif
---
+++ rpl/dispatchers/set_dispatcher.hpp
@@ -0,0 +1,22 @@
+#ifndef rpl_set_dispatcher_hpp
+#define rpl_set_dispatcher_hpp
+
+#include "dispatchers/dispatcher.hpp"
+#include "parameters.hpp"
+#include "rpl_environment.hpp"
+#include <functional>
+
+struct set_dispatcher : public dispatcher<std::string, std::function<void (double)>>
+{
+    set_dispatcher(rpl_environment& env) {
+        add(par::emitter_time,   [&]( double value ) { env.set_emitter_time(value); });
+        add(par::collector_time, [&]( double value ) { env.set_collector_time(value); });
+        add(par::scatter_time,   [&]( double value ) { env.set_scatter_time(value); });
+        add(par::gather_time,    [&]( double value ) { env.set_gather_time(value); });
+        add(par::resources,      [&]( double value ) { env.set_res(value); });
+        add(par::dimension,      [&]( double value ) { env.set_dim(value); });
+        //add(par::arch,           [&]( double value ) { env.set_emitter_time(value); });
+    }
+};
+
+#endif
