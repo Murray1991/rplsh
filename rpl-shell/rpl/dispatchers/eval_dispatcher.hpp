da1df6250623e69da4d93cc90698597bf67cd4be
Leonardo Gazzarri <leogazza91@gmail.com>
2017-05-28 19:22:18 +0200

    rename in eval_dispatcher

---
+++ rpl-shell/rpl/dispatchers/eval_dispatcher.hpp
@@ -0,0 +1,22 @@
+#ifndef rpl_eval_dispatcher_hpp
+#define rpl_eval_dispatcher_hpp
+
+#include "dispatcher.hpp"
+#include "visitors/visitors.hpp"
+#include "visitors/evaluators/evaluators.hpp"
+#include "parameters.hpp"
+#include <memory>
+
+struct eval_dispatcher : public dispatcher<std::string, std::unique_ptr<eval_visitor>>
+{
+    eval_dispatcher(rpl_environment& env) {
+        //add(par::show_default   ,  std::unique_ptr<printer>(new printer()));
+        add(par::servicetime    ,  std::unique_ptr<eval_visitor>(new servicetime(env)));
+        add(par::latency        ,  std::unique_ptr<eval_visitor>(new latencytime(env)));
+        add(par::compltime      ,  std::unique_ptr<eval_visitor>(new completiontime(env)));
+        add(par::pardegree      ,  std::unique_ptr<eval_visitor>(new pardegree(env)));
+        add(par::resources      ,  std::unique_ptr<eval_visitor>(new resources(env)));
+    }
+};
+
+#endif
