7b7a9204e8880f5526a40f5c27c941e88ecf2dd5
Leonardo Gazzarri <leogazza91@gmail.com>
2017-06-09 15:54:16 +0200

    utils functions

--- rpl-shell/rpl/utils/utils.hpp
+++ rpl-shell/rpl/utils/utils.hpp
@@ -2,6 +2,7 @@
 #define rpl_utils_hpp
 
 #include <string>
+#include <cmath>
 
 namespace utils {
 
@@ -15,6 +16,23 @@ namespace utils {
         }
         str.erase( str.find_last_not_of('0') + offset, std::string::npos);
         return str;
+    }
+
+    template <typename T>
+    std::size_t count_digits(const T& n) {
+        if ( n == 0 ) return 1;
+        return std::floor( std::log10(n) + 1 );
+    }
+
+    template <typename T>
+    std::string get_spaces(const T& num) {
+        return num > 0 ? std::string(num, ' ') : "";
+    }
+
+    template <typename T>
+    std::string get_idx(const T& idx, const T& max_idx) {
+        std::size_t num_spaces = count_digits(max_idx) - count_digits(idx);
+        return get_spaces(num_spaces) + "[" + std::to_string(idx) + "]";
     }
 
 }
